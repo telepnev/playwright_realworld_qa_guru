43bcfc40209a8ed387bd3cf9ce54bfacb76b5742
Telepnev Evgeny <telepnef@gmail.com>
2024-09-27 14:33:24 +0300

    Удаление статьи после ее создания

--- src/pages/articlePage.js
+++ src/pages/articlePage.js
@@ -9,10 +9,14 @@ export class ArticlePage extends BasePage{
         this.writeYourArticle = this.page.getByPlaceholder('Write your article (in markdown)');
         this.articleTags = this.page.getByPlaceholder('Enter tags');
         this.publishArticleButton = this.page.getByRole('button', { name: 'Publish Article' });
+        this.deleteArticleButton = this.page.getByRole('button', { name: 'Delete Article' }).first();
+        this.editArticleButton = this.page.getByRole('button', { name: ' Edit Article' }).first();
+        this.updateArticleButton = this.page.getByRole('button', { name: ' Update Article' });
+        
 
     }
 
-    async createNewArticle(articleTitle, articleAbout, writeYourArticle, articleTags) {
+    async createArticle(articleTitle, articleAbout, writeYourArticle, articleTags) {
         await this.articleTitle.click();
         await this.articleTitle.fill(articleTitle);
         await this.articleAbout.click();
@@ -24,4 +28,40 @@ export class ArticlePage extends BasePage{
         await this.publishArticleButton.click();
 
     }
+
+    async editArticle(articleTitle, articleAbout, writeYourArticle) {
+        await this.articleTitle.click();
+        await this.articleTitle.clear();
+        await this.articleTitle.fill(articleTitle);
+        await this.articleAbout.click();
+        await this.articleAbout.clear();
+        await this.articleAbout.fill(articleAbout);
+        await this.writeYourArticle.click();
+        await this.writeYourArticle.clear();
+        await this.writeYourArticle.fill(writeYourArticle);
+        await this.articleTags.click();
+        await this.updateArticleButton.click();
+
+    }
+
+    async toDeleteArticleButton() {
+        await this.deleteArticleButton.click();
+    }
+
+    async toEditArticleButton() {
+        await this.editArticleButton.click();
+    }
+
+    async toUpdateArticleButton() {
+        await this.updateArticleButton.click();
+    }
+
+    async confirmDeletionArticle() {
+            this.page.once('dialog', dialog => {
+            console.log(`Dialog message: ${dialog.message()}`);
+            dialog.dismiss().catch(() => {});
+          });
+    }
+
+    
 }
--- tests/articleTests.spec.js
+++ tests/articleTests.spec.js
@@ -9,9 +9,9 @@ let articleHelper;
 test.describe.only('Article tests', () => {
     test.beforeEach('Create User', async ({ page }) => {
       // todo перенести в отдельный класс
-        let userName = "telep";
-        let userEmail = "mail23@mk.ri";
-        let userPassword = "1234567";
+      let userName = "telep";
+      let userEmail = "mail23@mk.ri";
+      let userPassword = "1234567";
         const mainPage = new MainPage(page);
         const loginPage = new LoginPage(page);
         
@@ -26,6 +26,10 @@ test.describe.only('Article tests', () => {
             articleTitle : faker.food.dish(),
             articleAbout : faker.food.ethnicCategory(),
             writeArticle : faker.food.description(),
+            newArticleTitle : faker.food.dish(),
+            newArticleAbout : faker.food.ethnicCategory(),
+            newArticleWRite : faker.food.description(),
+
             getTag : () => {
                 const tags = ["AngelWings","Baklava","Bánh","BánhBao",
                     "BánhCăn","Sbiten","Okroshka","Semolinaporridge",
@@ -40,6 +44,49 @@ test.describe.only('Article tests', () => {
 
       test('Создание новой статьи', async ({ page }) => {
        
+        let createTitle = articleHelper.articleTitle;
+        let createArticleAbout = articleHelper.articleAbout;
+        let createWriteArticle = articleHelper.writeArticle;
+        let createTags = articleHelper.getTag();
+
+        const mainPage = new MainPage(page);
+        const articlePage = new ArticlePage(page);
+
+        await mainPage.goToNewArticle();
+        await articlePage.createArticle(createTitle, createArticleAbout, createWriteArticle, createTags);
+
+        await expect(page.getByRole('heading')).toContainText(createTitle);
+        await expect(page.locator(".article-content")).toContainText(createWriteArticle);
+        await expect(page.locator(".tag-list")).toContainText(createTags);
+
+      });
+
+      test('Редактирование статьи после ее создания', async ({ page }) => {
+       
+        let title = articleHelper.articleTitle;
+        let articleAbout = articleHelper.articleAbout;
+        let writeArticle = articleHelper.writeArticle;
+        let tags = articleHelper.getTag();
+
+        let newTitle = articleHelper.newArticleTitle;
+        let newArticleAbout = articleHelper.newArticleAbout;
+        let newWriteArticle = articleHelper.newArticleWRite;
+
+        const mainPage = new MainPage(page);
+        const articlePage = new ArticlePage(page);
+
+        await mainPage.goToNewArticle();
+        await articlePage.createArticle(title, articleAbout, writeArticle, tags);
+
+        await articlePage.toEditArticleButton();
+        await articlePage.editArticle(newTitle, newArticleAbout, newWriteArticle);
+        
+        await expect(page.getByRole('heading')).toContainText(newTitle);
+        await expect(page.locator(".article-content")).toContainText(newWriteArticle);
+      });
+
+      test('Удаление статьи после ее создания', async ({ page }) => {
+       
         let title = articleHelper.articleTitle;
         let articleAbout = articleHelper.articleAbout;
         let writeArticle = articleHelper.writeArticle;
@@ -49,13 +96,13 @@ test.describe.only('Article tests', () => {
         const articlePage = new ArticlePage(page);
 
         await mainPage.goToNewArticle();
-        await articlePage.createNewArticle(title, articleAbout, writeArticle, tags);
+        await articlePage.createArticle(title, articleAbout, writeArticle, tags);
 
-        await expect(page.getByRole('heading')).toContainText(title);
-        await expect(page.locator(".article-content")).toContainText(writeArticle);
-        await expect(page.locator(".tag-list")).toContainText(tags);
-
+        page.on('dialog', dialog => dialog.accept());
+        await articlePage.toDeleteArticleButton();
+      
+        await expect(page.getByRole('main')).toContainText('Articles not available.');
+     
       });
-
 
     });
